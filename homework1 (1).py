e6259956a55d4daf477511ad0fd93d6b1c53225f
acakir13 <36598680+acakir13@users.noreply.github.com>
2020-12-22 17:03:45 +0800

    Create homework1 (1).py
    22.12.2020

---
+++ homework1 (1).py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+x1=input('Lütfen Birinci Değeri Giriniz:')
+x2=input('Lütfen İkinci Değeri Giriniz:')
+x3=input('Lütfen Üçincü Değeri Giriniz:')
+x4=input('Lütfen Dördüncü Değeri Giriniz:')
+x5=input('Lütfen Beşinci Değeri Giriniz:')
+bilgiler = [x1,x2,x3,x4,x5]
+print (f'Girdiğiniz Değerler {bilgiler}')
+
+
+# In[ ]:
+
+
+x11=type(x1)
+x22=type(x2)
+x33=type(x3)
+x44=type(x4)
+x55=type(x5)
+print('Birinci Değerin Türü:{}'.format((x11)))
+print('İkinci Değerin Türü:{}'.format(x22))
+print('Üçüncü Değerin Türü:{}'.format(x33))
+print('Dördüncü Değerin Türü:{}'.format(x44))
+print('Beşinci Değerin Türü:{}'.format(x55))
+
+
+# In[ ]:
+
+
+
+
